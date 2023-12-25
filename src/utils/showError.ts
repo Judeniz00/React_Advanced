8516c7c38934014139ad92b0cc9e46935b5c6aa4
Deniz Uku <150124685+Judeniz00@users.noreply.github.com>
2023-12-25 21:02:40 +0300

    Delete src directory

--- src/utils/showError.ts
+++
@@ -1,7 +0,0 @@
-import { message } from "antd";
-
-const showError = (errorMessage: string) => {
-  message.error(errorMessage);
-};
-
-export default showError;
--- src/utils/showSuccess.ts
+++
@@ -1,7 +0,0 @@
-import { message } from "antd";
-
-const showSuccess = (successMessage: string) => {
-  message.success(successMessage);
-};
-
-export default showSuccess;
